770c7a2b2f6106ce908acb4e242192282b7624ed
LearXD <50178057+LearXD@users.noreply.github.com>
2022-06-28 15:36:15 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install unzip
+
+unzip PHP-7.0.14_Linux-x84_64.zip
+unlink PHP-7.0.14_Linux-x84_64.zip
+
+sudo chmod 777 ./install.sh
+
+echo "Instalação realizada com sucesso, use \"./install.sh\"!"
